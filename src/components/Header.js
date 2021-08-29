6b863c50c94355df12c33a2c4bdea0bc0f6c68f9
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-08-29 19:04:07 +0300

    ok

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Header extends React.Component
+{
+  render()
+  {
+    return(
+      <>
+      <header>
+      <h1>Lab 01</h1>
+      </header>
+      </>
+    )
+  }
+}
+
+export default Header
